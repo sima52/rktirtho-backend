30ecb92d55c19630c7a03f2d145a7735ba5c4457
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-03 22:56:49 +0600

    Training class defined

--- src/main/java/com/rktirtho/model/Education.java
+++ src/main/java/com/rktirtho/model/Education.java
@@ -4,6 +4,7 @@ public class Education {
 	private long id;
 	private String degree;
 	private String university;
+	private String subject;
 	private float cgpa;
 	private float resultScale;
 	private String latterGrade;
