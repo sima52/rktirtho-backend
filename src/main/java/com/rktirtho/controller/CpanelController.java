125306fcd1cbe871a21f86016aa8e681076aa008
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-03 23:10:41 +0600

    dashboard link created

--- src/main/java/com/rktirtho/controller/CpanelController.java
+++ src/main/java/com/rktirtho/controller/CpanelController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("cpanel")
 public class CpanelController {
 	
+	@RequestMapping("")
+	public String dashboard() {
+		return "cpanel-login";
+	}
+	
 	@RequestMapping("login")
 	public String login() {
 		return "cpanel-login";
